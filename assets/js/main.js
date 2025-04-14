b1d3e09ae667d656676e20c28da8694f2aace0bf
Ramiro Fernandez <pulgaramiro@gmail.com>
2025-04-14 15:41:23 -0300

    Highlighs  e.Button con <ul> del boton

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
     /* const buttonTimeframes = document.querySelectorAll('.timeframes__item'); */
     const buttonTimeframes = document.querySelectorAll('.user__timeframes')[0];
+    const timeFramesItem = document.querySelectorAll('.timeframes__item');
 
     buttonTimeframes.addEventListener('click', (e) => {
+        console.log(buttonTimeframes);
         if (e.target.tagName === "BUTTON") {
-            console.log(e.target.innerText);
-            console.log(e.target.dataset.action);
+            timeFramesItem.forEach(b => b.classList.remove('timeframes__item--high'));
+            e.target.classList.add('timeframes__item--high');
         }
+
+
     });
 
 
