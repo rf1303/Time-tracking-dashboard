8c0bed219554469d882a2335470d7f9c81fb16d6
Ramiro Fernandez <pulgaramiro@gmail.com>
2025-04-14 12:44:36 -0300

    Delegacion e.Button con elemento Padre <ul> del boton

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,24 +3,27 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log("Inicio");
 
     /* const buttonTimeframes = document.querySelectorAll('.timeframes__item'); */
-    const buttonTimeframes = document.querySelectorAll('.user__timeframes');
+    const buttonTimeframes = document.querySelectorAll('.user__timeframes')[0];
 
-    buttonTimeframes.forEach(button => {
-        button.addEventListener('click', (e) => {
-            /* let highLight =  e.target.dataset.action; */
-            if (e.target.tagName === "BUTTON") {
-                console.log(e.target.innerText);
-            }
-        });
+    buttonTimeframes.addEventListener('click', (e) => {
+        if (e.target.tagName === "BUTTON") {
+            console.log(e.target.innerText);
+            console.log(e.target.dataset.action);
+        }
     });
 
 
-    // buttonTimeframes.addEventListener('click', (e) => {
-    //     if (e.target.tagName === "BUTTON") {
-    //         console.log(e.target.innerText);
-    //         console.log(e.target.dataset.action);
-    //     }
+
+
+    // buttonTimeframes.forEach(button => {
+    //     button.addEventListener('click', (e) => {
+    //         /* let highLight =  e.target.dataset.action; */
+    //         if (e.target.tagName === "BUTTON") {
+    //             console.log(e.target.innerText);
+    //         }
+    //     });
     // });
+
 
     /* console.log(buttonTimeframes.data); */
 });
