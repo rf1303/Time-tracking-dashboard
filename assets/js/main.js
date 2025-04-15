d581a64a05bde1eff5b39ea04c274bf1f3f2ed0e
Ramiro Fernandez <pulgaramiro@gmail.com>
2025-04-15 09:57:42 -0300

    fetch o await back

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,28 @@
+let datos = {};
+async function cargarDatos() {
+    try {
+    const response = await fetch('../../data.json');
+    let datos = await response.json();
+    console.log("DATOS con await:", datos);
+    } catch (error) {
+    console.error("Error cargando datos.json:", error);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-  
-    console.log("Inicio");
+    
+    cargarDatos();
+    console.log('Datos des DOM : ', datos);
+    // const fetch = fetch("../../data.json",);
+    // fetch('../../data.json')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         datos = data;
+    //         console.log(data);
+    //         console.log(datos);
+    //     })
+    //     .catch(error => console.error("Error cargando datos.json:", error));
+    // console.log("DATOS : ", datos);
 
     /* const buttonTimeframes = document.querySelectorAll('.timeframes__item'); */
     const buttonTimeframes = document.querySelectorAll('.user__timeframes')[0];
@@ -17,8 +39,27 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
 
+//
+// console.log(fetchPromise);
+//
+// fetchPromise.then((response) => {
+//   console.log(`Received response: ${response.status}`);
+// });
+
+// console.log("Started request…");
+//
+// fetchPromise.then((response) => {
+//   const jsonPromise = response.json();
+//   jsonPromise.then((data) => {
+//     console.log(data[0].name);
+//   });
+// });
 
 
+
+    // daily = yesterday 
+    // week = last week
+    // monthly = last mounth
     // buttonTimeframes.forEach(button => {
     //     button.addEventListener('click', (e) => {
     //         /* let highLight =  e.target.dataset.action; */
